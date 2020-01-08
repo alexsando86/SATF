224363a32529b772db50f13f564fdd83d32bcfbb
김채민/UI개발팀/193531 <193531@ssg.global>
2020-01-08 17:15:09 +0900

    imageTransform Add

---
+++ cra_satf/src/components/ImageTransform.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ImageTransform extends React.Component {
+  render() {
+    const { onCreateShow } = this.props;
+    return (
+      <section>
+        <div className="rect_test"></div>
+      </section>
+    );
+  }
+}
+
+export default ImageTransform;
