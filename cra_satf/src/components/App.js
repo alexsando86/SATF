fb2b8a99b4fa0fa81fe0a52f2d98695b28b1b08b
김채민/UI개발팀/193531 <193531@ssg.global>
2020-01-08 17:22:22 +0900

    test

--- cra_satf/src/components/App.js
+++ cra_satf/src/components/App.js
@@ -1,50 +1,57 @@
 import React from 'react';
 import '../styles/common.scss';
-import Aside from "./Aside";
-import Header from "./Header";
-import { gsap } from "gsap";
+import Aside from './Aside';
+import Header from './Header';
+import ImageTransform from './ImageTransform';
+import { gsap } from 'gsap';
 
 class App extends React.Component {
-    state = {
-        visible: false,
-    };
+  state = {
+    visible: false,
+  };
 
-    constructor(props) {
-        super(props);
-        this.circleRef = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.circleRef = React.createRef();
+  }
 
-    componentDidMount() {
-        gsap.to(this.circleRef.current, {
-            rotation: 360,
-            duration:1,
-            backgroundColor: 'red'
-        })
-    }
+  componentDidMount() {
+    gsap.to(this.circleRef.current, {
+      rotation: 360,
+      duration: 1,
+      backgroundColor: 'red',
+    });
+  }
 
-    handleToggleControl = () => {
-        this.setState({
-            visible: !this.state.visible,
-        })
-    };
-    style = {
-        border: '1px solid #000',
-        borderRadius: '20px',
-        width: '100px',
-        height: '100px',
-        margin: 'auto'
-    }
-    render() {
-        return(
-            <div className="App">
-                <Header onCreateShow={this.handleToggleControl} />
-                <section className="container">
-                    <Aside currentState={this.state} onCreateHide={this.handleToggleControl} />
-                    <div ref={this.circleRef} style={this.style}>11</div>
-                </section>
-            </div>
-        )
-    }
+  handleToggleControl = () => {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  };
+  style = {
+    border: '1px solid #000',
+    borderRadius: '20px',
+    width: '100px',
+    height: '100px',
+    margin: 'auto',
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header onCreateShow={this.handleToggleControl} />
+        <section className="container">
+          <Aside
+            currentState={this.state}
+            onCreateHide={this.handleToggleControl}
+          />
+          <div ref={this.circleRef} style={this.style}>
+            11
+          </div>
+          <ImageTransform />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
