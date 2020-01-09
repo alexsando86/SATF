92236498f5b88cd9ce53d87128562689b61df5ce
193531 <chaemin.kim@ssg.com>
2020-01-09 16:58:45 +0900

    gsap test

--- cra_satf/src/components/Aside.js
+++ cra_satf/src/components/Aside.js
@@ -1,52 +1,63 @@
 import React from 'react';
 
 class Aside extends React.Component {
-    render() {
-        const { currentState, onCreateHide } = this.props;
-        return(
-            <div className={currentState.visible === true ? 'aside active' : 'aside' }>
-                <ul className="aside_ctg">
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                    <li>Button</li>
-                    <li>Loading</li>
-                    <li>Festival</li>
-                    <li>Snow</li>
-                </ul>
-                <button type="button" className="aside_close" onClick={onCreateHide}><span className="blind">닫기</span></button>
-            </div>
-        )
-    }
+  render() {
+    const { currentState, onCreateHide } = this.props;
+
+    // onSubPageShow = () => {
+
+    // };
+
+    return (
+      <div className={currentState.visible === true ? 'aside active' : 'aside'}>
+        <ul className="aside_ctg">
+          <li>
+            <button type="button" /*onClick={this.onSubPageShow}*/>
+              Button
+            </button>
+          </li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+          <li>Button</li>
+          <li>Loading</li>
+          <li>Festival</li>
+          <li>Snow</li>
+        </ul>
+        <button type="button" className="aside_close" onClick={onCreateHide}>
+          <span className="blind">닫기</span>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Aside;
--- cra_satf/src/components/ImageTransform.js
+++ cra_satf/src/components/ImageTransform.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { gsap } from 'gsap';
 
 class ImageTransform extends React.Component {
+  constructor(props) {
+    super(props);
+    this.rectRef = React.createRef();
+  }
+
+  onBackgroundChg = () => {
+    gsap.to(this.rectRef.current, {
+      duration: 3,
+      backgroundColor: '#ff687a',
+    });
+  };
   render() {
-    const { onCreateShow } = this.props;
     return (
-      <section>
-        <div className="rect_test"></div>
+      <section class="test_conts">
+        <div className="rect_test" ref={this.rectRef}></div>
+        <button
+          type="button"
+          className="btn_test"
+          onClick={this.onBackgroundChg}
+        >
+          Background Change
+        </button>
       </section>
     );
   }
