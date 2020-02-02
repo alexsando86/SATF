fbd1152dc6073cb63bf727c41af5ae50b519a9b0
alexsando86 <alexsando86@github.com>
2020-02-02 22:30:19 +0900

    'counter.js'

--- cra_satf/src/components/App.js
+++ cra_satf/src/components/App.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import '../styles/common.scss';
-import Aside from "./Aside";
-import Header from "./Header";
+import Aside from './Aside';
+import Header from './Header';
+import Counter from './Counter';
 
 class App extends React.Component {
-    componentDidMount(){
-        console.log('DiDMount')
-    }
+  componentDidMount() {
+    console.log('DiDMount');
+  }
 
-    state = {
-        visible: false,
-        IScroll : null,
-    };
+  state = {
+    visible: false,
+    IScroll: null,
+  };
 
-    handleToggleControl = () => {
-        this.setState({
-            visible: !this.state.visible,
-        })
-    };
-    render() {
-        return(
-            <div className="App">
-                <Header onCreateShow={this.handleToggleControl} />
-                <section className="container">
-                    <Aside currentState={this.state} onCreateHide={this.handleToggleControl} />
-                </section>
-            </div>
-        )
-    }
+  handleToggleControl = () => {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header onCreateShow={this.handleToggleControl} />
+        <section className="container">
+          <Aside
+            currentState={this.state}
+            onCreateHide={this.handleToggleControl}
+          />
+          <Counter />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ cra_satf/src/components/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { gsap } from 'gsap';
+
+class Counter extends React.Component {
+  state = {};
+  render() {
+    return <div className="counter">0</div>;
+  }
+}
+export default Counter;
