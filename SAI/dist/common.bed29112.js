bd85b4320e359e32092ff5c440263c3cb0c18c8e
184744@ssg.com <184744@ssg.com>
2020-04-08 16:41:34 +0900

    'menu_add'

--- SAI/dist/common.bed29112.js
+++ SAI/dist/common.bed29112.js
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61278" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61650" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
