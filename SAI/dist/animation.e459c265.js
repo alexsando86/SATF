168534a98c31d2f1960316ffd7bc2fb75a3d2d69
alexsando86 <alexsando86@github.com>
2020-05-17 21:48:01 +0900

    'cubicBezier'

--- SAI/dist/animation.e459c265.js
+++ SAI/dist/animation.e459c265.js
@@ -266,7 +266,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3998" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3617" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
