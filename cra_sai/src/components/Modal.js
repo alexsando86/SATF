9fc878be1516061a29e9fb35e152b34e2beaf36b
184744@ssg.com <184744@ssg.com>
2020-05-28 17:03:00 +0900

    'modal'

--- cra_sai/src/components/Modal.js
+++ cra_sai/src/components/Modal.js
@@ -1,34 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const Modal = () => {
+	const saiModalBtn = useRef([]);
+	const saiModalWrap = useRef(null);
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		let data = ["modal1", "modal2", "modal3", "modal4", "modal5"];
+		saiModalBtn.current = new Array(data.length);
+		setData(data);
+	}, []);
+
 	function addActiveClass(e) {
 		const self = e.target.dataset.modalCount;
-		document.querySelector(".sai_modal_wrap").classList.add(self, "active");
+		saiModalWrap.current.className = 'sai_modal_wrap';
+		saiModalWrap.current.classList.add(self, "active");
+	}
+
+	function removeActiveClass(e) {
+		let self = e.target;
+		if (!self.classList.contains("sai_modal_cts")) {
+			saiModalWrap.current.classList.add("out");
+		}
 	}
 
 	return (
 		<React.Fragment>
 			<div className="sai_contents">
 				<div className="sai_modal_btnset">
-					<button type="button" className="sai_modal_btn" data-modal-count="modal1" onClick={addActiveClass}>
-						Modal1
-					</button>
-					<button type="button" className="sai_modal_btn" data-modal-count="modal2">
-						Modal2
-					</button>
-					<button type="button" className="sai_modal_btn" data-modal-count="modal3">
-						Modal3
-					</button>
-					<button type="button" className="sai_modal_btn" data-modal-count="modal4">
-						Modal4
-					</button>
-					<button type="button" className="sai_modal_btn" data-modal-count="modal5">
-						Modal5
-					</button>
+					{data.map((el, i) => (
+						<button key={i} type="button" className="sai_modal_btn" data-modal-count={el} ref={saiModalBtn.current[i]} onClick={addActiveClass}>
+							{el}
+						</button>
+					))}
 				</div>
 			</div>
-			<div className="sai_modal_wrap">
-				<div className="sai_modal">
+			<div className="sai_modal_wrap" ref={saiModalWrap}>
+				<div className="sai_modal" onClick={removeActiveClass}>
 					<div className="sai_modal_cts">안녕하세요 SAI Modal 입니다.</div>
 				</div>
 			</div>
