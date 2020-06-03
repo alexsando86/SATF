62205619a43d86b27f6c1503bfbdad52b05addcf
184744@ssg.com <184744@ssg.com>
2020-06-03 16:16:02 +0900

    'logoadding'

--- cra_sai/src/components/Header.js
+++ cra_sai/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SAILOGO from "../images/common/logo_sai.png";
 
 function Header() {
 	return (
 		<div className="sai_header">
 			<div className="sai_header_logo">
-				<Link to="/Home">SAI HOME</Link>
+				<Link to="/Home">
+					<img src={SAILOGO} alt="" />
+				</Link>
 			</div>
 		</div>
 	);
